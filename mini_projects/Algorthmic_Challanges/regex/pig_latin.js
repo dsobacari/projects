5a90dc1864e92d76600a6447ab020da2001e2244
dsobacari <dsobacari@gmail.com>
2020-10-06 11:06:24 -0400

    commit

---
+++ mini_projects/Algorthmic_Challanges/regex/pig_latin.js
@@ -0,0 +1,15 @@
+//Pig Latin is a way of altering English Words. The rules are as follows:
+//- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+//- If a word begins with a vowel, just add "way" at the end.
+
+function translatePigLatin(str) {
+    let startsWithVowel= /^[A,E,I,O,U]/gi;
+    let startsWithCons= /^([^A,E,I,O,U]*)/gi;
+    return str.match(startsWithVowel) ? str + 'way' : 
+     str.replace(startsWithCons, '') + str.match(startsWithCons) + 'ay';
+  }
+  //translatePigLatin("paragraphs")
+  //translatePigLatin("california");
+  //translatePigLatin("eight")
+  //translatePigLatin("schwartz")
+  translatePigLatin("rhythm")
